4b24037d86088777c2d75a97b68bcc5404f6acf3
Paulinho <paulinhosiri@hotmail.com>
2015-04-08 20:29:54 -0300

    Data inicial e data final para o filtro em vendas adicionados

---
+++ br/com/livraria/filter/VendaFilter.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.livraria.filter;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Paulinho
+ */
+public class VendaFilter {
+    private Date dataInicial;
+    private Date dataFinal;
+
+    public Date getDataFinal() {
+        return dataFinal;
+    }
+
+    public void setDataFinal(Date dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+
+    public Date getDataInicial() {
+        return dataInicial;
+    }
+
+    public void setDataInicial(Date dataInicial) {
+        this.dataInicial = dataInicial;
+    }
+    
+    
+    
+}
