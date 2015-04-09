8890056a3d2bad2ae7006b1e5de6be0b5f6d37b6
Paulinho <paulinhosiri@hotmail.com>
2015-04-09 18:48:39 -0300

    Validações para os campos nome, nome da editora, data, descricão, quantidade, preço e autor. Métodos toString, hashCode e equals adicionados

--- br/com/livraria/domain/Livro.java
+++ br/com/livraria/domain/Livro.java
@@ -9,48 +9,71 @@ import java.math.BigDecimal;
 import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  *
  * @author Paulinho
  */
-
 @Entity
-@Table(name="tbl_livros")
-@NamedQueries({@NamedQuery(name="Livro.listar", query = "SELECT livro FROM Livro livro"),
-@NamedQuery(name = "Livro.buscarPorCodigo", query = "SELECT livro FROM Livro livro WHERE livro.codigo = :codigo")})
+@Table(name = "tbl_livros")
+@NamedQueries({
+    @NamedQuery(name = "Livro.listar", query = "SELECT livro FROM Livro livro"),
+    @NamedQuery(name = "Livro.buscarPorCodigo", query = "SELECT livro FROM Livro livro WHERE livro.codigo = :codigo")})
 public class Livro {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "cd_livro")
     private Long codigo;
-    
-    @Column(name = "nm_nome", length = 50 , nullable = false)
+ 
+    @NotEmpty(message = "O campo nome é obrigatório")
+    @Column(name = "nm_nome", length = 50, nullable = false)
     private String nome;
-    
+
+    @NotEmpty(message = "O campo editora é obrigatório")
     @Column(name = "nm_editora", length = 50, nullable = false)
     private String nomeEditora;
     
+    @NotEmpty(message = "O campo data da edição e obrigatório")
     @Column(name = "dt_edicao", nullable = false)
     private Date data;
-    
+
+    @Size(min = 5, max = 50, message = "Tamanho inválido para o campo descrição (5 - 50)")
     @Column(name = "ds_livro", length = 50, nullable = false)
     private String descricao;
-    
+
+    @NotNull(message = "O campo quantidade é obrigatório")
+    @Min(value = 0, message = "Informe um valor maior ou igual a zero para o campo quantidade")
+    @Max(value = 9999, message = "Informe um valor menor que dez mil para o campo quantidade")
     @Column(name = "qt_livro", nullable = false)
     private Integer quantidade;
-    
+
+    @NotNull(message = "O campo preço é obrigatório")
+    @DecimalMin(value = "0.00", message = "Informe um valor maior ou igual a zero para o campo preço")
+    @DecimalMax(value = "99999,99", message = "Infome um valor menor que 100 mil para o campo preço")
+    @Digits(integer = 5, fraction = 2, message = "Informe um valor válido para o campo preço")
     @Column(name = "vl_preco", precision = 7, scale = 2, nullable = false)
     private BigDecimal preco;
-    
+
+    @NotNull(message = "O campo autor é obrigatório")
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "tbl_autores_cd_autor", referencedColumnName = "cd_autor", nullable = false)
     private Autor autor;
 
@@ -118,7 +141,35 @@ public class Livro {
         this.autor = autor;
     }
     
-    
-    
-    
+    @Override
+    public String toString() {
+        return "Livro [codigo=" + codigo + ", nome=" + nome + ", nomeEditora=" + nomeEditora + ", data=" + data + ", descricao=" + descricao + ", quantidade=" + quantidade + ", preco=" + preco + ", autor=" + autor + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) 
+            return true;
+        if (obj == null) 
+            return false;       
+        if (getClass() != obj.getClass()) 
+            return false;        
+        Livro other = (Livro) obj;
+        if (codigo == null) {
+            if (other.codigo != null) 
+                return false;            
+        } else if (!codigo.equals(other.codigo)) 
+            return false;        
+        return true;
+
+    }
+
 }
